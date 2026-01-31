c33ca2c7c9adce9d8f19caf3fa492832b6055cb2
勿忘心安 <36999228+xkatld@users.noreply.github.com>
2026-02-01 01:39:26 +0800

    Update lxdapiuserserver.php

--- Fmis/zjmf/lxdapiuserserver/lxdapiuserserver.php
+++ Fmis/zjmf/lxdapiuserserver/lxdapiuserserver.php
@@ -266,7 +266,6 @@ function lxdapiuserserver_CreateAccount($params)
                 'domainstatus' => 'Active',
                 'username'     => 'root',
                 'dedicatedip'  => $params['server_ip'],
-                'bwlimit'      => (int)($configoptions['traffic_limit'] ?? 100),
             ];
             
             Db::name('host')->where('id', $params['hostid'])->update($update);
